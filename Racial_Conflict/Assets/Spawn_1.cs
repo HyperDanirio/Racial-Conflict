855283dfdb694f271a0aac5a5cbbb1e255a4722e
HyperDanirio <hyperdanirio@protonmail.com>
2023-03-09 20:34:28 +0800

    Daniel_03/09/2023_02
    Started Spawning only 1 object tho

---
+++ Racial_Conflict/Assets/Spawn_1.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_1 : MonoBehaviour
+{
+    public GameObject objectToSpawn; 
+    private int object_to_spawn=0;
+    public int max_object_to_spawn=1;
+
+    void Start()
+    {
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.X)){
+            SpawnObject();
+        }
+    }
+    public void SpawnObject()
+{
+    if(object_to_spawn<max_object_to_spawn){
+    Vector3 spawnPosition = new Vector3(1, 0, 0); 
+    Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
+    object_to_spawn++;
+
+    }
+    
+}
+}
