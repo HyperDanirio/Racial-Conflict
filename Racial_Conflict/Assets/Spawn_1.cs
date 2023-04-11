10c0758a2055607955c4d56d48ea79b95c8e1ee0
HyperDanirio <hyperdanirio@protonmail.com>
2023-04-11 13:07:49 +0800

    Daniel_Update_0411

--- Racial_Conflict/Assets/Spawn_1.cs
+++ Racial_Conflict/Assets/Spawn_1.cs
@@ -7,6 +7,8 @@ public class Spawn_1 : MonoBehaviour
     public GameObject objectToSpawn; 
     private int object_to_spawn=0;
     public int max_object_to_spawn=1;
+    public int spawn_location_x;
+    public int spawn_location_y;
 
     void Start()
     {
@@ -20,7 +22,7 @@ public class Spawn_1 : MonoBehaviour
     public void SpawnObject()
 {
     if(object_to_spawn<max_object_to_spawn){
-    Vector3 spawnPosition = new Vector3(-10, 0, 0); 
+    Vector3 spawnPosition = new Vector3(spawn_location_x, spawn_location_y, 0); 
     Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
     object_to_spawn++;
 
