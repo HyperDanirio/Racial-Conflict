bce6b1d4046fa74cf8e906fb682044d2ee7f0706
K1T20 <56916807+bangme4@users.noreply.github.com>
2023-04-18 13:10:01 +0800

    haha
    attack 2 taliin yvlaga

---
+++ Racial_Conflict/Assets/Spawn_2.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_2 : MonoBehaviour
+{
+    public GameObject objectToSpawn; 
+    private int object_to_spawn=0;
+    public int max_object_to_spawn=1;
+    public int spawn_location_x;
+    public int spawn_location_y;
+
+    void Start()
+    {
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.K)){
+            SpawnObject();
+        }
+    }
+    public void SpawnObject()
+{
+    if(object_to_spawn<max_object_to_spawn){
+    Vector3 spawnPosition = new Vector3(spawn_location_x, spawn_location_y, 0); 
+    Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
+    object_to_spawn++;
+
+    }
+    
+}
+}
