9b9a7226b63f9538acf0286ba53b7fe2994011fb
K1T20 <56916807+bangme4@users.noreply.github.com>
2023-04-25 13:22:24 +0800

    okey but no bitches

--- Racial_Conflict/Assets/T2_Movement_Melee.cs
+++ Racial_Conflict/Assets/T2_Movement_Melee.cs
@@ -5,14 +5,58 @@ using UnityEngine;
 public class T2_Movement_Melee : MonoBehaviour
 {
     public float speed;
+    public float attackRange = 2f;
+    public float attackDelay = 1f;
+    public int damage = 10;
+
+    private GameObject playerSoldier;
+    private bool attacking = false;
+
     void Start()
     {
-        
+        playerSoldier = GameObject.FindGameObjectWithTag("Soldier_Team_1");     
     }
 
     void Update()
     {
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        float distance = Vector3.Distance(transform.position, playerSoldier.transform.position);
+
+        if (distance <= attackRange && !attacking)
+        {
+            // Stop moving when player soldier is in attack range
+            StopMoving();
+            StartCoroutine(Attack());
+        }
+        else
+        {
+            // Move in one direction if player soldier is not in attack range
+            MoveRight();
+        }
     }
 
+    IEnumerator Attack()
+    {
+        attacking = true;
+
+        yield return new WaitForSeconds(attackDelay);
+
+        float distance = Vector3.Distance(transform.position, playerSoldier.transform.position);
+        if (distance <= attackRange)
+        {
+            Debug.Log("Enemy took " + damage + " damage!");
+        }
+
+        attacking = false;
+    }
+
+    void MoveRight()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    void StopMoving()
+    {
+        // Stop moving by setting the speed to 0
+        speed = 0;
+    }
 }
